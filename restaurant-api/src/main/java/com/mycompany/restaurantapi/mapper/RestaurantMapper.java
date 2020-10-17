88541067518c8876e8d511a2ab303e63f15728a1
Ivan Franchin <ivan.franchin@takeaway.com>
2020-10-17 11:23:22 +0200

    project update - mapstruct - update to mapstruct version 1.4.1.Final; - simplify RestaurantMapper.java configuration.

--- restaurant-api/src/main/java/com/mycompany/restaurantapi/mapper/RestaurantMapper.java
+++ restaurant-api/src/main/java/com/mycompany/restaurantapi/mapper/RestaurantMapper.java
@@ -5,28 +5,26 @@ import com.mycompany.restaurantapi.rest.dto.CreateRestaurantDto;
 import com.mycompany.restaurantapi.rest.dto.RestaurantDto;
 import com.mycompany.restaurantapi.rest.dto.UpdateRestaurantDto;
 import com.mycompany.restaurantapi.service.CityService;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(
         componentModel = "spring",
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
-        uses = {CityService.class}
+        uses = CityService.class,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR
 )
-public abstract class RestaurantMapper {
+public interface RestaurantMapper {
 
-    @Autowired
-    private CityService cityService;
+    RestaurantDto toRestaurantDto(Restaurant restaurant);
 
-    public abstract RestaurantDto toRestaurantDto(Restaurant restaurant);
+    @Mapping(target = "city", source = "cityId")
+    Restaurant toRestaurant(CreateRestaurantDto createRestaurantDto);
 
-    @Mapping(target = "city", expression = "java(cityService.validateAndGetCity(createRestaurantDto.getCityId()))")
-    public abstract Restaurant toRestaurant(CreateRestaurantDto createRestaurantDto);
-
-    @Mapping(target = "city", expression = "java(updateRestaurantDto.getCityId() == null ? restaurant.getCity() : cityService.validateAndGetCity(updateRestaurantDto.getCityId()))")
-    public abstract void updateRestaurantFromDto(UpdateRestaurantDto updateRestaurantDto, @MappingTarget Restaurant restaurant);
+    @Mapping(target = "city", source = "cityId")
+    void updateRestaurantFromDto(UpdateRestaurantDto updateRestaurantDto, @MappingTarget Restaurant restaurant);
 
 }
