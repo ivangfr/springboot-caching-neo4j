0e2b18dc143b1ae6e607ef527b0a3adfdaddf685
Ivan Franchin <ivan.franchin@takeaway.com>
2021-05-22 15:00:33 +0200

    project update - update spring-native to 0.10.0-SNAPSHOT; - convert application.yml to application.properties; - remove yaml from spring-aot-maven-plugin and from native configuration in spring-boot-maven-plugin; - as spel is needed, it is not ignored anymore in native configuration in spring-boot-maven-plugin; - generate missing configuration for native image using Tracing Agent; - update README.

--- restaurant-api/src/main/java/com/mycompany/restaurantapi/RestaurantApiApplication.java
+++ restaurant-api/src/main/java/com/mycompany/restaurantapi/RestaurantApiApplication.java
@@ -1,8 +1,25 @@
 package com.mycompany.restaurantapi;
 
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.springframework.aop.SpringProxy;
+import org.springframework.aop.framework.Advised;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.DecoratingProxy;
+import org.springframework.nativex.hint.ProxyHint;
+import org.springframework.nativex.hint.TypeHint;
 
+import javax.cache.annotation.CacheMethodDetails;
+
+@TypeHint(types = {
+        CacheMethodDetails.class,
+        LoggerConfig.class
+})
+@ProxyHint(types = {
+        SpringProxy.class,
+        Advised.class,
+        DecoratingProxy.class
+})
 @SpringBootApplication
 public class RestaurantApiApplication {
 
